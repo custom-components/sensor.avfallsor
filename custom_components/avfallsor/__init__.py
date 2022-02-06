5ae3b5b1fe0a46f196da01506f6bae2c5352444e
Hellowlol <Hellowlol@users.noreply.github.com>
2022-02-06 15:21:16 +0100

    bump

--- custom_components/avfallsor/__init__.py
+++ custom_components/avfallsor/__init__.py
@@ -6,7 +6,7 @@ from homeassistant.core import Config, HomeAssistant
 
 DOMAIN = "avfallsor"
 NAME = DOMAIN
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 ISSUEURL = "https://github.com/hellowlol/sensor.avfallsor/issues"
 
 STARTUP = """
